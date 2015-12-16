85424502b7d8bf60fe6e09c506b06a71a1b072de
kimo72 <pastalion@hotmail.com>
2015-12-16 00:34:21 -0500

    New-VirtualMachine
    This script is meant to create virtual machines rapidly, by selecting
    everything with parameters

---
+++ New-VirtualMachine.ps1
@@ -0,0 +1,52 @@
+﻿
+
+
+$myVM = "AD2"
+$myNetwork = "wifi"
+$myVHDpath = 'C:\Virtual Machines\AD2.vhdx'
+$myParentpath = "C:\Virtual Machines\Bases\Base Windows Server 2012 R2 Dat x64 GUI - English.vhdx"
+
+$VmName
+$VirtualSwitch
+$VHDPath 
+$parentPath
+$generation 
+$staticMemory
+$ProcessorCount
+
+
+
+
+function New-VirtualMachine{
+
+ Param
+    (
+
+[parameter(Mandatory=$true)]
+$VmName,
+
+$VirtualSwitch,
+[parameter(Mandatory=$true)]
+$VHDPath,
+
+[parameter(Mandatory=$true)] 
+$ParentPath,
+
+[parameter(Mandatory=$true)]
+[ValidateSet(1,2)]
+[int]
+$Generation, 
+
+$StaticMemory,
+
+[int]$ProcessorCount
+    
+)
+
+
+$VHDX = New-VHD -Path $VHDpath -ParentPath $ParentPath -Differencing 
+$VmName = New-VM -Name $VmName -MemoryStartupBytes 1GB -Generation $generation -VHDPath $VHDPath
+$VmName | Get-VMNetworkAdapter | Connect-VMNetworkAdapter -SwitchName $virtualSwitch
+$VmName | Set-VM -ProcessorCount $ProcessorCount -StaticMemory $staticMemory
+}
+New-VirtualMachine -VmName 1 -VirtualSwitch wifi -VHDPath "C:\Virtual Machines" -ParentPath C:\Virtual Machines\Bases
